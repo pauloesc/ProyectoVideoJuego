d4d53240a1261a2490fac03c8702806b5fde2ca2
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-03 20:46:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,211 +1,203 @@
 #include <iostream>
+
 #include <string>
+
 using namespace std;
 
 /*
  cambiar la manera en que se inicializa DtPartidaIndividual
  */
 
-
-
 int main() {
 
-	InstanciaControladorUsuario;
-    
+        InstanciaControladorUsuario;
 
-    while (!termina) {
-        
-        try{
-        
-		cout << "Eliga la operacion a realizar: "<<'\n';
-		cout << "1) Alta de usuario." <<'\n';
-		cout << "2) Iniciar sesión." <<'\n';
-		cout << "3) Modificar fecha del sistema." <<'\n';
-		cout << "4) Cargar datos de prueba." <<'\n';
-		cout << "5) Salir." <<'\n';
+        while (!termina) {
 
-		cin >> eleccion;
-		cout <<'\n';
+                try {
 
+                        cout << "Eliga la operacion a realizar: " << '\n';
+                        cout << "1) Alta de usuario." << '\n';
+                        cout << "2) Iniciar sesión." << '\n';
+                        cout << "3) Modificar fecha del sistema." << '\n';
+                        cout << "4) Cargar datos de prueba." << '\n';
+                        cout << "5) Salir." << '\n';
 
-		switch(eleccion) {
+                        cin >> eleccion;
+                        cout << '\n';
 
-			case 1: {
+                        switch (eleccion) {
 
-				string vemail;
-				cout << "ingrese email:" <<'\n';
-				cin.ignore();
-				getline(cin,vemail);
-				cout <<'\n';
+                        case 1: {
 
-				string vpassword;
-				cout << "ingrese contrasenia:" <<'\n';
-				cin.ignore();
-				getline(cin,vpassword);
-				cout <<'\n';
+                                string vemail;
+                                cout << "ingrese email:" << '\n';
+                                cin.ignore();
+                                getline(cin, vemail);
+                                cout << '\n';
 
-				InstanciaControladorUsuario.IngresardatosUsuario(vemail,vpassword);
+                                string vpassword;
+                                cout << "ingrese contrasenia:" << '\n';
+                                cin.ignore();
+                                getline(cin, vpassword);
+                                cout << '\n';
 
-				bool esDesarollador=0;
-				cout << "Es un usuario desarollador: 1 para si, 0 para no" <<'\n';
-				cin.ignore();
-				getline(cin,esDesarollador);
-				
-				if(esDesarollador) {
-					InstanciaControladorUsuario.IngresardatosDesarrollador(nomEmpresa:string)
-				}
-				else{
+                                InstanciaControladorUsuario.IngresardatosUsuario(vemail, vpassword);
 
-					bool auxWhile=1;
-					while(auxWhile){
+                                bool esDesarollador = 0;
+                                cout << "Es un usuario desarollador: 1 para si, 0 para no" << '\n';
+                                cin.ignore();
+                                getline(cin, esDesarollador);
 
-						string vnickname;
-						cout << "ingrese nickname:" <<'\n';
-						cin.ignore();
-						getline(cin,vnickname);
-						cout <<'\n';
+                                if (esDesarollador) {
+                                        InstanciaControladorUsuario.IngresardatosDesarrollador(nomEmpresa: string)
+                                } else {
 
-						string vdescripcion;
-						cout << "ingrese descripcion:" <<'\n';
-						cin.ignore();
-						getline(cin,vdescripcion);
-						cout <<'\n';
+                                        bool auxWhile = 1;
+                                        while (auxWhile) {
 
-						bool verificacion;
-						verificacion:= InstanciaControladorUsuario.IngresardatosJugador(vnickname, vdescripcion)
+                                                string vnickname;
+                                                cout << "ingrese nickname:" << '\n';
+                                                cin.ignore();
+                                                getline(cin, vnickname);
+                                                cout << '\n';
 
-						//si puede registrarse con ese usuario
-						if(verificacion){
-							auxWhile = 0;
-						}
-						else{
+                                                string vdescripcion;
+                                                cout << "ingrese descripcion:" << '\n';
+                                                cin.ignore();
+                                                getline(cin, vdescripcion);
+                                                cout << '\n';
 
-							bool vreintenarIngresoDatos;
-							cout << "No es posible registar dichs datos quiere volver a intentarlo: 0 para no, 1 para si" <<'\n';
-							cin.ignore();
-							getline(cin,vreintenarIngresoDatos);
-							cout <<'\n';
+                                                bool verificacion;
+                                                verificacion: = InstanciaControladorUsuario.IngresardatosJugador(vnickname, vdescripcion)
 
-							 auxWhile = vreintenarIngresoDatos;
+                                                //si puede registrarse con ese usuario
+                                                if (verificacion) {
+                                                        auxWhile = 0;
+                                                } else {
 
-						}
+                                                        bool vreintenarIngresoDatos;
+                                                        cout << "No es posible registar dichs datos quiere volver a intentarlo: 0 para no, 1 para si" << '\n';
+                                                        cin.ignore();
+                                                        getline(cin, vreintenarIngresoDatos);
+                                                        cout << '\n';
 
+                                                        auxWhile = vreintenarIngresoDatos;
 
-					}
+                                                }
 
-				}
+                                        }
 
-				bool vconfirmar;
-				cout << "Desea confirmar inscripcion 1 para si 0 para no:" <<'\n';
-				cin.ignore();
-				getline(cin,vconfirmar);
-				cout <<'\n';
+                                }
 
-				if(vconfirmar){
-					InstanciaControladorUsuario.cancelarAltaUsuario()
-				}
-				else{
-					InstanciaControladorUsuario.ConfirmarAltaUsuario()
-				}
-			}
-			break;
+                                bool vconfirmar;
+                                cout << "Desea confirmar inscripcion 1 para si 0 para no:" << '\n';
+                                cin.ignore();
+                                getline(cin, vconfirmar);
+                                cout << '\n';
 
+                                if (vconfirmar) {
+                                        InstanciaControladorUsuario.cancelarAltaUsuario()
+                                } else {
+                                        InstanciaControladorUsuario.ConfirmarAltaUsuario()
+                                }
+                        }
+                        break;
 
-			case 2: {
-				
-				auxWhile=1;
-				while(auxWhile){
+                        case 2: {
 
-					string vemail;
-					cout << "ingrese email : "<<'\n';
-					cin.ignore();
-					getline(cin,vemail);
-					cout <<'\n';
+                                auxWhile = 1;
+                                while (auxWhile) {
 
-					string passw;
-					cout << "ingrese contrasenia : "<<'\n';
-					cin.ignore();
-					getline(cin,passw);
-					cout <<'\n';
-					
-					bool ingreso = 0;
-					ingreso:= InstanciaControladorUsuario.IngresarDatos(vemail;passw);
+                                        string vemail;
+                                        cout << "ingrese email : " << '\n';
+                                        cin.ignore();
+                                        getline(cin, vemail);
+                                        cout << '\n';
 
-					//si no ingreso
-					if(!ingreso){				
-						bool reintentar;
-						cout << "Fallo el ingreso. Quiere reintentar ingresar?: 1 para si 0 para no"<<'\n';
-						cin.ignore();
-						getline(cin,reintentar);
-						cout <<'\n';
+                                        string passw;
+                                        cout << "ingrese contrasenia : " << '\n';
+                                        cin.ignore();
+                                        getline(cin, passw);
+                                        cout << '\n';
 
-						auxWhile = reintentar
-					}
-					//si ingreso
-					else{ auxWhile = 0;}
+                                        bool ingreso = 0;
+                                        ingreso: = InstanciaControladorUsuario.IngresarDatos(vemail; passw);
 
+                                        //si no ingreso
+                                        if (!ingreso) {
+                                                bool reintentar;
+                                                cout << "Fallo el ingreso. Quiere reintentar ingresar?: 1 para si 0 para no" << '\n';
+                                                cin.ignore();
+                                                getline(cin, reintentar);
+                                                cout << '\n';
 
-				}
-			}
-			break;
+                                                auxWhile = reintentar
+                                        }
+                                        //si ingreso
+                                        else {
+                                                auxWhile = 0;
+                                        }
 
-			case 3: {
+                                }
+                        }
+                        break;
 
-                int an,me,di,ho,mi;
-                
-                cout << "Es necesario ingresar la fecha y hora del sistema" <<'\n';
-                
-                cout << "ingrese el año: ";
-                cin >> an;
-                cout <<'\n';
+                        case 3: {
 
-                cout << "ingrese el mes: ";
-                cin >> me;
-                cout <<'\n';
+                                int an, me, di, ho, mi;
 
-                cout << "ingrese el dia: ";
-                cin >> di;
-                cout <<'\n';
+                                cout << "Es necesario ingresar la fecha y hora del sistema" << '\n';
 
-                cout << "ingrese la hora: ";
-                cin >> ho;
-                cout <<'\n';
+                                cout << "ingrese el año: ";
+                                cin >> an;
+                                cout << '\n';
 
-                cout << "ingrese el minuto: ";
-                cin >> mi;
-                cout <<'\n';
+                                cout << "ingrese el mes: ";
+                                cin >> me;
+                                cout << '\n';
 
-			}
-			break;
+                                cout << "ingrese el dia: ";
+                                cin >> di;
+                                cout << '\n';
 
-			case 4: {
-				
-					cout << "CARGA DE DATOS DE PRUEBA";
-					cout <<'\n';
-				 }
-			break;
+                                cout << "ingrese la hora: ";
+                                cin >> ho;
+                                cout << '\n';
 
-			case 5: {
-					cout << "Chau";
-					cout <<'\n';
-					termina = 1;
-				}
+                                cout << "ingrese el minuto: ";
+                                cin >> mi;
+                                cout << '\n';
 
-			break;
+                        }
+                        break;
 
-			default: {
-				
-				termina = true;
-				cout << "numero invalido, cerrando sistema..."<<'\n';
-				}
-			break;
-		}
-       }
-	 
-	catch (invalid_argument& e) {
-            cout << e.what() <<endl;
+                        case 4: {
+
+                                cout << "CARGA DE DATOS DE PRUEBA";
+                                cout << '\n';
+                        }
+                        break;
+
+                        case 5: {
+                                cout << "Chau";
+                                cout << '\n';
+                                termina = 1;
+                        }
+
+                        break;
+
+                        default: {
+
+                                termina = true;
+                                cout << "numero invalido, cerrando sistema..." << '\n';
+                        }
+                        break;
+                        }
+                } catch (invalid_argument & e) {
+                        cout << e.what() << endl;
+                }
         }
-	}
 
-	return 0;
+        return 0;
 }
