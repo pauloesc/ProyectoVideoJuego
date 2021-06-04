13a80892df6d1530bba2e75da2da26706853b0ae
Daniel Alejandro Garcia Miranda <daniel.alejandro.garcia@fing.edu.uy>
2021-06-04 02:25:39 -0300

    Upload New File

---
+++ include/Jugador.h
@@ -0,0 +1,41 @@
+#ifndef JUGADOR
+#define JUGADOR
+
+#include <map>
+#include <vector>
+
+#include "Usuario.h"
+#include "Partida.h" //este tiene a Videojuego.h creo
+#include "Suscripcion.h"
+#include "DtPartidaIndividual.h"
+#include "DtPartidaEnCurso.h"
+
+
+class Jugador : public Usuario {
+	private:
+		string nickname;
+		string descripcion;
+		map <int, Partida *> partidas;
+		vector<Suscripcion> sus;
+	public:
+		Jugador (string unNickname, string unaDescripcion);
+		string getnickname ();
+		string getdescripcion();
+
+		set(DtPartidaIndividual) darPartidasIndividualesFinalizadas(string nombrevid);
+		set(DtPartidaEnCurso) darPartidasEnCurso();
+		Partida encontrarPartidasIndividual(int codigoAnterior);
+		void AsociarPartidaIniciada(Partida PI);
+		void DesvincularPartida(Partida par);
+		void finalizarPartida(int identificador);
+		
+		void eliminarSuscripciones(string nomVJ);
+		set(Suscripcion) darSuscripcionesActivas();
+		bool tieneSuscripcionActiva(string nombrevid);
+		bool tieneSuscripcionActiva(Videojuego vid);
+		void cancelarSuscripcionActiva(string Juego);
+		void asociarSuscripcion(Suscripcion SNuevo);
+};
+
+
+#endif
