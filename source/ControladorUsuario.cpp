6d87298e5db638242a9392b227f3906c7a0b508e
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-05 15:25:53 -0300

    Update ControladorUsuario.cpp

--- source/ControladorUsuario.cpp
+++ source/ControladorUsuario.cpp
@@ -1,8 +1,11 @@
 #include <string> 
+#include ControladorUsuario.h
+#include Usuario.h
 
 using namespace std;
 
-ControladorUsuario *ControladorUsuario::instance = NULL; 
+ControladorUsuario* ControladorUsuario::instance = NULL; 
+ControladorUsuario::ControladorUsuario() {}
 
 ControladorUsuario *ControladorUsuario::getInstancia() {
  if (instance == NULL)
@@ -10,6 +13,12 @@ ControladorUsuario *ControladorUsuario::getInstancia() {
  return instance;
 } 
 
+static string email;
+static string contrasenia;
+static string nomEmpresa;
+static bool desarrollador;
+static string nickname;
+static string desc;
 
 
 void ControladorUsuario::IngresardatosUsuario(string email, string contrasenia){
@@ -23,5 +32,39 @@ void ControladorUsuario::IngresardatosDesarrollador(string nomEmpresa){
 }
 
 bool ControladorUsuario::IngresardatosJugador(string nickname, string desc){
-    
+       bool existe=false;
+       vector<Usuario*>::iterator it = Usuarios.begin();
+       While ((!existe) && (it!=suarios.end()){
+              if (dynamic_cast<Jugador*>(it)){  
+                  if (it->getnickname()==nickname)
+                     existe=true;
+              }
+             ++it;
+       }
+       if(!existe){
+              ControladorUsuario::desarrollador=false; 
+              ControladorUsuario::nickname=nickname;
+              ControladorUsuario::desc=desc;
+       }
+       return (!existe);
 }
+
+void ControladorUsuario::ConfirmarAltaUsuario(){
+      
+       if (ControladorUsuario::desarrollador){
+              Desarrollador* d= new Desarrollador (ControladorUsuario::email, ControladorUsuario::contrasenia,  ControladorUsuario::nomEmpresa)
+              Usuario u*=dynamic_cast <*Usurio>(d); //no se si esto es necesario
+       }
+       else {
+              Desarrollador* d= new Desarrollador (ControladorUsuario::email, ControladorUsuario::contrasenia,  ControladorUsuario::nomEmpresa)
+              Usuario u*=dynamic_cast <*Usurio>(d); //no se si esto es necesario
+
+       }
+              
+
+usuarios.push_back (u);
+}
+void ControladorUsuario::cancelarAltaUsuario();
+
+Jugador* ControladorUsuario::darJugador(); 
+
