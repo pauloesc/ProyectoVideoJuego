0db52deb25b47af83e7c67ebe2f1aed56553c685
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-05 00:20:08 -0300

    Update ControladorUsuario.cpp

--- source/ControladorUsuario.cpp
+++ source/ControladorUsuario.cpp
@@ -1,3 +1,27 @@
 #include <string> 
 
 using namespace std;
+
+ControladorUsuario *ControladorUsuario::instance = NULL; 
+
+ControladorUsuario *ControladorUsuario::getInstancia() {
+ if (instance == NULL)
+ instance = new ControladorUsuario();
+ return instance;
+} 
+
+
+
+void ControladorUsuario::IngresardatosUsuario(string email, string contrasenia){
+       ControladorUsuario::email=email;
+       ControladorUsuario::contrasenia=contrasenia;
+}
+
+void ControladorUsuario::IngresardatosDesarrollador(string nomEmpresa){
+       ControladorUsuario::nomEmpresa=nomEmpresa;
+       ControladorUsuario::derrollador=true;
+}
+
+bool ControladorUsuario::IngresardatosJugador(string nickname, string desc){
+    
+}
