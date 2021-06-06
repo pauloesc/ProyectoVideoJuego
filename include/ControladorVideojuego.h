e158ed4d54e1637cbdf8ec889c466d2d1940de7f
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-06 00:29:34 -0300

    Update ControladorVideojuego.h

--- include/ControladorVideojuego.h
+++ include/ControladorVideojuego.h
@@ -2,16 +2,44 @@
 #define CONTROLADORVIDEOJUEGO
 
 #include ".../include/Videojuego.h"
-using namespace std;
 #include <vector>
+#include <string>
+
+using namespace std;
 
 class ControladorVideojuego {
 
 private:
 static ControladorVideojuego * instance;
-ControladorUsuario();
-vector<VideoJuego*> Videojuegos;
+ControladorVideojuego();
+vector<Videojuego*> Videojuegos;
+static string nombreJuego;
+static string descrip;
+static float costoMensual;
+static float costoTrimestral;
+static float costoAnual;
+static float costoVitalicia;
+static DtCategoria categoria;
+static vector<string> cats;
+
 
 public:
 static ControladorUsuario * getInstance();
 
+void datosVideojuego(string nombreJuego, string descrip, float costoMensual, float costoTrimestral, float costoAnual, float costoVitalicia); //publicar videojuego
+vector<DtCategoria> ObtenerCategoriaPlataforma();
+vector<DtCategoria> ObtenerCategoriaGenero();
+vector<DtCategoria> ObtenerCategoriaOtros(); 
+void agregarcategoria (string categoria);
+DtVideojuego ObtenerInfoVideojuego();
+void ConfirmarAltavideoJuego();
+void CancelarAltavideoJuego();
+string getTipo();
+Videojuego* darVideojuego(string Juego); 
+void seleccionarVideoJuego(string nomVJ); 
+vector<string> obtenerVideoJuegosConTodasLasPartidasFinalizadas();
+void cancelarLaEliminación();
+void eliminarVideoJuego();
+vector<string> busquedaVideojuegos(string criterio);
+
+}
