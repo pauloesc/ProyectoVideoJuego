caa1074ff91bb8c53ea85b99dda0d42afa590488
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-06 01:57:00 -0300

    Update ControladorVideojuego.cpp

--- source/ControladorVideojuego.cpp
+++ source/ControladorVideojuego.cpp
@@ -1,7 +1,9 @@
 #include <string> 
 #include <vector>
-#include ".../include/ControladorVideojuego.h
-#include Videojuego.h
+#include ".../include/ControladorVideojuego.h"
+#include ".../include/DtVideojuego.h"
+#include ".../include/DtCategoria.h"
+#include ".../include/Videojuego.h"
 
 using namespace std;
 
@@ -23,13 +25,50 @@ float ControladorVideojuego::costoVitalicia=0;
 
 void ControladorVideojuego::datosVideojuego(string nombreJuego, string descrip, float costoMensual, float costoTrimestral, float costoAnual, float costoVitalicia){
 
+    ControladorVideojuego::nombreJuego=nombreJuego;
+    ControladorVideojuego::descrip=descrip;
+    ControladorVideojuego::costoMensual=costoMensual;
+    ControladorVideojuego::costoTrimestral=costoTrimestral;
+    ControladorVideojuego::costoAnual=costoAnual;
+    ControladorVideojuego::costoVitalicia=costoVitalicia;
+
 }
 
-vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaPlataforma();
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaPlataforma(){
+    
+    vector<DtCategoria> colplataforma; 
+    int tamCol=Categorias.size();
+    for (int i=0; i<tamCol; i++){
+        if(Categorias[i]->getTipo()=plataforma){  //mirarlo despues en categoria
+            colplataforma.push_back(Categorias[i]->ObtenerDataCategoria()); //mirarlo en cat
+        }
+    }
+    return colplataforma;
+}
 
-vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaGenero();
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaGenero(){
+    
+    vector<DtCategoria> colgenero; 
+    int tamCol=Categorias.size();
+    for (int i=0; i<tamCol; i++){
+        if(Categorias[i]->getTipo()=genero){  //mirarlo despues en categoria
+            colgenero.push_back(Categorias[i]->ObtenerDataCategoria());
+        }
+    }
+    return colgenero;
+}
 
-vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaOtros(); 
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaOtros(){
+
+    vector<DtCategoria> colotro; 
+    int tamCol=Categorias.size();
+    for (int i=0; i<tamCol; i++){
+        if(Categorias[i]->getTipo()=otro){  //mirarlo despues en categoria
+            colotro.push_back(Categorias[i]->ObtenerDataCategoria());
+        }
+    }
+    return colotro;
+} 
 
 
 
