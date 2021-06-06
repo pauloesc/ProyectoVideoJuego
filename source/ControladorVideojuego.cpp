90064326b1ac12f600ef32abe8f381e7f68833b2
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-06 00:47:23 -0300

    Update ControladorVideojuego.cpp

--- source/ControladorVideojuego.cpp
+++ source/ControladorVideojuego.cpp
@@ -1,6 +1,6 @@
 #include <string> 
 #include <vector>
-#include ControladorVideojuego.h
+#include ".../include/ControladorVideojuego.h
 #include Videojuego.h
 
 using namespace std;
@@ -8,8 +8,28 @@ using namespace std;
 ControladorVideojuego* ControladorVideojuego::instance = NULL; 
 ControladorVideojuego::ControladorVideojuego() {}
 
-ControladorVideojuego *ControladorVideojuego::getInstancia() {
+ControladorVideojuego* ControladorVideojuego::getInstancia() {
  if (instance == NULL)
  instance = new ControladorVideojuego();
  return instance;
 } 
+
+string ControladorVideojuego::nombreJuego=0;
+string ControladorVideojuego::descrip=0;
+float ControladorVideojuego::costoMensual=0;
+float ControladorVideojuego::costoTrimestral=0;
+float ControladorVideojuego::costoAnual=0;
+float ControladorVideojuego::costoVitalicia=0;
+
+void ControladorVideojuego::datosVideojuego(string nombreJuego, string descrip, float costoMensual, float costoTrimestral, float costoAnual, float costoVitalicia){
+
+}
+
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaPlataforma();
+
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaGenero();
+
+vector<DtCategoria> ControladorVideojuego::ObtenerCategoriaOtros(); 
+
+
+
