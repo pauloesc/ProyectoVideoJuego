780d390e835290349877cfd3d5bf56939be00fe3
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-07 01:46:02 -0300

    Add new file

---
+++ source/ControladorEstadistica.cpp
@@ -0,0 +1,51 @@
+ControladorEstadistica* ControladorEstadistica::instance = NULL; 
+
+
+ControladorEstadistica* ControladorEstadistica::getInstance() {
+ if (instance == NULL)
+ instance = new ControladorEstadistica();
+ return instance;
+} 
+
+
+vector<string> ControladorEstadistica::JuegosDelDesarollador(){
+ 
+    ControladorUsuario* ControlUsuario;
+    ControlUsuario = ControladorUsuario::getInstance();
+
+    Desarrollador* InstanciaDesarollador;
+    InstanciaDesarollador = ControlUsuario.darDesarrollador();
+    
+    string nombDes;
+    nombDes = InstanciaDesarollador->getEmail();
+    
+    ControladorVideojuego* ControlVideojuego;
+    ControlVideojuego = ControladorUsuario::getInstance();
+ 
+    vector<string>  infoRetornar;
+    infoRetornar = ControlVideojuego->DarJuegos(nombDes);
+    
+    return infoRetornar;
+    
+}
+
+vector<DataEstadisticaDeSegundoTipo> SeleccionEstadisticasDeCiertoJuego(string nombreJuego){
+    
+    ControladorUsuario* ControlUsuario;
+    ControlUsuario = ControladorUsuario::getInstance();
+    
+    Desarrollador* InstanciaDesarollador;
+    InstanciaDesarollador = ControlUsuario.darDesarrollador();
+    
+    ControladorVideojuego* ControlVideojuego;
+    ControlVideojuego = ControladorUsuario::getInstance();
+    
+    Videojuego* vj =0;
+    vj = ControlVideojuego->darVideoJuego(nombreJuego);
+    
+    vector<DataEstadisticaDeSegundoTipo> vectorRetornar;
+    vectorRetornar = InstanciaDesarollador.DarEstardisticas(vj);
+    
+    return vectorRetornar;
+    
+}
