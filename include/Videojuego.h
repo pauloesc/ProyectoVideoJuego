6fa0e17e0efde73f7e2ba327ba8742156041df0f
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-06 21:04:18 -0300

    Add new file

---
+++ include/Videojuego.h
@@ -0,0 +1,43 @@
+#ifndef VideoJuego
+#define VideoJuego
+using namespace std;
+
+class VideoJuego {
+private:
+    
+    string Nombre;
+    string Descripcion;
+    float CostoVitalicio;
+    float CostoMensual;
+    float CostoTrimestral;
+    float CostoAnual;
+    
+    vector<Desarrollador*> VectorDePunterosDesarolladores;
+    vector<Parda*> VectorDePunterosPartidasDelVideojuego;
+    
+    //recordar que esto podria llegar a dar algun tipo de problemas si alguien gurdase la direccion de memeoria de los puntajes, ya que cuando se redimensiona vector cambia de lugar en la memoria del vector(pregunta a Paulo si no entienden).
+    vector<Puntaje> VectorDeObjetosPuntaje;
+	
+public:
+	VideoJuego(string Nombre, string Descripcion, float CostoVitalicio, float CostoMensual, float CostoTrimestral, float CostoAnual);
+
+    string getNombre();
+    string getDescripcion();
+    float getCostoVitalicio();
+    float getCostoMensual();
+    float getCostoTrimestral();
+    float getCostoAnual();
+    
+    //falta implementra esta 
+    //no encuentro donde esta dafinida(no se que hace la funcion)
+    sDesarrollador(des:Desarrollador);
+    
+    bool TodasFinalizadas();
+    EliminarPartidas();
+    eliminarPuntajes();
+    float totalHorasJugadas();
+    
+    ~VideoJuego();
+    
+};
+#endif
