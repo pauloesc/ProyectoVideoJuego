7e81452c8ec94fb231523dfe769d858ae839f754
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-06 21:05:08 -0300

    Add new file

---
+++ source/Videojuego.cpp
@@ -0,0 +1,96 @@
+VideoJuego::VideoJuego(string Nombre, string Descripcion, float CostoVitalicio, float CostoMensual, float CostoTrimestral, float CostoAnual); {
+    this->Nombre = Nombre;
+    this->Descripcion = Descripcion;
+    this->CostoVitalicio = CostoVitalicio;
+    this->CostoMensual = CostoMensual;
+    this->CostoTrimestral = CostoTrimestral;
+    this->CostoAnual = CostoAnual;
+}
+
+
+string VideoJuego::getNombre(){
+    return Nombre;
+}
+
+string VideoJuego::getDescripcion(){
+    return Descripcion;
+}
+
+string VideoJuego::getCostoVitalicio(){
+    return CostoVitalicio;
+}
+
+
+float VideoJuego::CostoMensual(){
+    return CostoMensual;
+}
+
+float VideoJuego::CostoTrimestral(){
+    return CostoTrimestral;
+}
+
+float VideoJuego::CostoAnual(){
+    return CostoAnual;
+}
+
+
+
+VideoJuego::sDesarrollador(des:Desarrollador){
+    
+}
+
+bool VideoJuego::TodasFinalizadas(){
+    
+    bool Todasfinalizadas = 1;
+    int i = 0;
+    while( Todasfinalizadas && ( i < VectorDePunterosPartidasDelVideojuego.size() ) ){
+        
+        Todasfinalizadas = (*VectorDePunterosPartidasDelVideojuego[i]).getEsPartidaEnCurso();
+        i=i+1;
+    }
+    return Todasfinalizadas;
+}
+
+VideoJuego::EliminarPartidas(){
+    
+    
+    for(int i=0; i < VectorDePunterosPartidasDelVideojuego.size(); i++ ){
+        
+        (*VectorDePunterosPartidasDelVideojuego[i]).eliminarPartida();
+    }
+    
+    //no se si es necesario
+    //elimino toda la info dentro del vector;
+    iterator1= VectorDePunterosVideojuegos.begin();
+    iterator2= VectorDePunterosVideojuegos.end();
+    VectorDePunterosVideojuegos.erase(iterator1, iterator2);
+    
+}
+
+
+VideoJuego::eliminarPuntajes(){
+    
+    iterator1= VectorDeObjetosPuntaje.begin();
+    iterator2= VectorDeObjetosPuntaje.end();
+    VectorDePunterosVideojuegos.erase(iterator1, iterator2);
+    
+}
+
+
+float VideoJuego::totalHorasJugadas(){
+    
+    float valorTotal = 0;
+    
+    for(int i=0; i < VectorDePunterosPartidasDelVideojuego.size(); i++ ){
+        
+        valorTotal = valorTotal + (*VectorDePunterosPartidasDelVideojuego[i]).tiempoTotal();
+    }
+    
+    return valorTotal;
+    
+}
+
+
+
+VideoJuego::~VideoJuego(){
+}
