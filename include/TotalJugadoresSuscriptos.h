bda907532bddb8c1a4095e900ca0b234daefa663
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-08 20:40:15 -0300

    Add new file

---
+++ include/TotalJugadoresSuscriptos.h
@@ -0,0 +1,15 @@
+#ifndef ESTADISTICATOTALJUGADORESSUSCRIPTOS
+#define ESTADISTICATOTALJUGADORESSUSCRIPTOS
+
+#include "ControladorUsuario.h"
+
+
+class TotalJugadoresSuscriptos : public Estadistica{
+private:
+
+public:
+	DataEstadisticaDeSegundoTipo DarEstadistica(VideoJuego* vjp);
+};
+
+#endif
+
