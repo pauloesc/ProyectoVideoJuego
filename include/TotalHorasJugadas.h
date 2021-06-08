2191b6db778f66cd04d87e1df1f9cf13390743d9
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-08 20:39:36 -0300

    Add new file

---
+++ include/TotalHorasJugadas.h
@@ -0,0 +1,15 @@
+#ifndef ESTADISTICATOTALHORASJUGADAS
+#define ESTADISTICATOTALHORASJUGADAS
+
+
+
+class TotalHorasJugadas : public Estadistica{
+private:
+
+public:
+	DataEstadisticaDeSegundoTipo DarEstadistica(VideoJuego* vjp);
+};
+
+
+
+#endif
