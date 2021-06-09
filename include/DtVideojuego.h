b6d836c69485d8bc405089359f429b41438363f7
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-08 23:39:58 -0300

    Update DtVideojuego.h

--- include/DtVideojuego.h
+++ include/DtVideojuego.h
@@ -16,24 +16,24 @@ private:
 	float costoTrimestral;
 	float costoAnual;
 	float costoVitalicio;
-	map<string, DtCategoria> categorias;
+	vector<string> categorias;
 public:
     DtVideojuego();
-	DtVideojuego(string titulo,string descripcion,float costoMensual,float costoTrimestral,float costoAnual, float costoVitalicio, map<string, DtCategoria> categorias);
+	DtVideojuego(string titulo,string descripcion,float costoMensual,float costoTrimestral,float costoAnual, float costoVitalicio, vector<string> categorias);
 	string getNombre();
 	string getDescripcion();
 	float getCostoMensual();
 	float getCostoTrimestral();
 	float getCostoAnual();
 	float getCostoVitalicio();
-	map<string, DtCategoria> getCategorias();
+	vector<string> getCategorias();
 	void setNombre(string nombre);
 	void setDescripcion(string descripcion);
 	void setCostoMensual(float costoMensual);
 	void setCostoTrimestral(float costoTrimestral);
 	void setCostoAnual(float costoAnual);
 	void setCostoVitalicio(float costoVitalicio);
-	void setCategorias(map<string, DtCategoria> categorias);
+	void setCategorias(vector<string> categorias);
 	friend ostream& operator<<(ostream &o, DtVideojuego *s);
 	virtual ~DtVideojuego();
 };
