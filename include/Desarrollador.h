26d737877102e749861ac2c7f2bf9802f58fe336
Daniel Alejandro Garcia Miranda <daniel.alejandro.garcia@fing.edu.uy>
2021-06-09 12:21:16 -0300

    Upload New File

---
+++ include/Desarrollador.h
@@ -0,0 +1,25 @@
+#ifndef DESARROLLADOR
+#define DESARROLLADOR
+
+#include <vector>
+
+#include "Usuario.h"
+#include "Videojuego.h"
+#include "DataEstadisticaDeSegundoTipo.h"
+#include "Estadistica.h"
+
+
+class Desarrollador : public Usuario {
+	private:
+		string nombreEmpresa;
+		vector <Estadistica *> estadisticas;
+	public:
+		Desarrollador (string unEmail, string unaContrasenia, string unaEmpresa);
+		string getnomEmpresa ();
+		vector<DataEstadisticaDeSegundoTipo *> DarEstadisticas(Videojuego *vj);
+		bool esJugador();
+        void getEstadisticas(vector<Estadistica *>);
+};
+
+
+#endif
