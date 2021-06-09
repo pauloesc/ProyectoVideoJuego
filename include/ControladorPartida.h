e6c488fee77e0315c01a46b35b6dd13acec72094
Adrian Martin Surraco Pereira <adrian.surraco.pereira@fing.edu.uy>
2021-06-09 00:33:00 -0300

    Abandonar partida multijugador

--- include/ControladorPartida.h
+++ include/ControladorPartida.h
@@ -45,6 +45,7 @@ public:
 
 	void eliminarPartidas(VideoJuego* vj);
 	vector<DtPartidaEnCurso*> obtenerPartidasEnCurso();
+	vector<DtPartidaEnCurso*> obtenerPartidasEnCursoUnido();
 	void finalizarPartida(int identidicador);
 	vector<string> listarVideojuegosConSuscripcionActiva();
 	vector<DtPartidaIndividual*> HistorialDePartidasIndividualesFinalizadas();
