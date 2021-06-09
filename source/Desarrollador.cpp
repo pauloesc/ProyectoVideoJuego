3264022eec4947f490de9cb2906473af554e5ea9
Daniel Alejandro Garcia Miranda <daniel.alejandro.garcia@fing.edu.uy>
2021-06-09 12:22:27 -0300

    Upload New File

---
+++ source/Desarrollador.cpp
@@ -0,0 +1,26 @@
+#include "Desarrollador.h"
+
+using namespace std;
+
+Desarrollador::Desarrollador (string unEmail, string unaContrasenia, string unaEmpresa) : Usuario (unEmail, unaContrasenia) {
+	nombreEmpresa = unaEmpresa;
+}
+
+string Desarrollador::getnomEmpresa() {
+	return nombreEmpresa;
+}
+
+bool Desarrollador::esJugador() {
+	return false;
+}
+
+vector<DataEstadisticaDeSegundoTipo *> Desarrollador::DarEstadisticas(Videojuego *vj) {
+	vector<DataEstadisticaDeSegundoTipo *> res;
+	for (int i = 0; i < estadisticas.size(); i++)
+		res.push_back(*estadisticas[i]->DarEstadisticas(vj));
+	return res;
+}
+
+void Desarrollador::getEstadisticas(vector<Estadistica *>) {
+    estadisticas = vector<Estadistica *>;
+}
