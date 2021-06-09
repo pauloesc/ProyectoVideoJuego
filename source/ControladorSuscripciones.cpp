fe89684b6afb1e7aecc163e57d7d36d5a87f875b
Daniel Alejandro Garcia Miranda <daniel.alejandro.garcia@fing.edu.uy>
2021-06-09 15:45:38 -0300

    Update ControladorSuscripciones.cpp

--- source/ControladorSuscripciones.cpp
+++ source/ControladorSuscripciones.cpp
@@ -1,27 +1,26 @@
 #include "ControladorSuscripciones.h"
 
-vector<DtSuscripcion> ControladorSuscripciones::obtenerSuscripcionesActivas() {
+vector<DtSuscripcion *> ControladorSuscripciones::obtenerSuscripcionesActivas() {
     ControladorUsusario* cu = ControladorUsuario::getInstance();
     Jugador* jug = cu->darJugador();
     return *jug->darSuscripcionesActivas();
 }
 
-vector<DtSuscripcion> ControladorSuscripciones::obtenerSuscripcionesNoActivas() {
-    vector<DtSuscripcion> susActivas =  obtenerSuscripcionesActivas();
-    vector<DtSuscripcion> res;
+vector<DtSuscripcion *> ControladorSuscripciones::obtenerSuscripcionesNoActivas() {
+    vector<DtSuscripcion *> susActivas =  obtenerSuscripcionesActivas();
+    vector<DtSuscripcion *> res;
     ControladorVideojuego* cvj = ControladorVideojuego::getInstance();
     vector<Videojuego*> vidoejue = cvj->DarVideojuegos();
-    videojue::iterator it;
-    for (it = videojue.begin(); it != videojue.end(); ++it)
-        res.push_back(it->crearDtsuscripcion()); //el it es asi o (*it) asi?
-    //Me falta hacer la diferencia de res y susActivas
+    for (int i = 0; i < videojue.size(); i++)
+        res.push_back(*videojue[i]->crearDtsuscripcion());
+    //Falta hacer la diferencia entre susActivas y res
 }
 
 void ControladorSuscripciones::SeleccionarVideojuego(string Juego) {
     ControladorVideojuego* cvj = ControladorVideojuego::getInstance();
     ControladorSuscripciones::vj = cvj->darVideojuego(Juego);
 }
-void ControladorSuscripciones::IngresarDatosSuscripcion(string tiposuscripcion, DtPago pag) {
+void ControladorSuscripciones::IngresarDatosSuscripcion(string tiposuscripcion, DtPago *pag) {
     ControladorSuscripciones::tiposus = tiposuscripcion;
     ControladorSuscripciones::pago = pag;
 }
