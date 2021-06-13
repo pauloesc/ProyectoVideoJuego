ece2f2cbad1c5070fb5cf65e28ae3a77000724d0
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-13 00:04:43 -0300

    Update DataEstadistica.cpp

--- source/DataEstadistica.cpp
+++ source/DataEstadistica.cpp
@@ -28,6 +28,6 @@ void DataEstadistica::setDescripcion(string des) {
 }
 
 ostream& operator<<(ostream &o, DataEstadistica* d) {
-  o << "Info Estadistica: " << '\n' << "Nombre: " << d->getNombre() << "Descripcion: "  << d->getDescripcion();
+  o << "Info Estadistica: " << '\n' << "Nombre: " << d->getNombre() <<" "<< "Descripcion: "  << d->getDescripcion()<<'\n';
   return o;
 }
