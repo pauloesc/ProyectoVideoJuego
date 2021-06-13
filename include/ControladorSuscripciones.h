2c936a8ed1de9e1599cf838294cc9831943c1883
Maria Alejandra Galetta Paz <maria.galetta@fing.edu.uy>
2021-06-13 18:54:21 -0300

    Update ControladorSuscripciones.h

--- include/ControladorSuscripciones.h
+++ include/ControladorSuscripciones.h
@@ -24,7 +24,7 @@ using namespace std;
 class ControladorSuscripciones {
     private:
         static ControladorSuscripciones* Instance;
-        
+        ControladorSuscripciones();
         Videojuego *vj;
         string nomJ;
         string tiposus;
