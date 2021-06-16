e4a011d8df0ae9067c3b826a421d628267ec89a1
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-16 18:49:13 -0300

    Update DataEstadistica.cpp

--- source/DataEstadistica.cpp
+++ source/DataEstadistica.cpp
@@ -28,6 +28,6 @@ void DataEstadistica::setDescripcion(string des) {
 }
 
 ostream& operator<<(ostream &o, DataEstadistica* d) {
-  o << "Info Estadistica: " << '\n' << "Nombre: " << d->getNombre() <<" "<< "Descripcion: "  << d->getDescripcion()<<'\n';
+  o << "Info Estadistica: " << endl << "Nombre: " << d->getNombre() << endl << "Descripcion: "  << d->getDescripcion()<< endl;
   return o;
 }
