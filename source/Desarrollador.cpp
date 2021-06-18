f303a245f328028e2418fb2de532c8b15e83c175
Paulo Elder SÃ¡nchez Cuello <paulo.sanchez@fing.edu.uy>
2021-06-17 22:42:13 -0300

    Update Desarrollador.cpp

--- source/Desarrollador.cpp
+++ source/Desarrollador.cpp
@@ -21,6 +21,37 @@ vector<DtEstadisticaDeSegundoTipo *> Desarrollador::DarEstadisticas(Videojuego *
 	return res;
 }
 
+/*
+esta funcion funcion pero solo hay que usarla si es que queremos que el desarollador pueda seguir ingresando estadisticas sin borrar las anteriores
+void Desarrollador::setEstadisticas(vector<Estadistica *> estadist) {
+
+	int max = estadist.size();
+
+	for( int i=0; i < max; i++ ){
+
+		Estadistica * eAgregar = estadist[i];
+
+		bool Encontrado = 0; 
+		int j = 0;
+		int max2 = estadisticas.size();
+
+		while ( (j < max2) && (!Encontrado) ){
+			
+			if( estadisticas[j] == eAgregar ){
+				Encontrado = 1;
+			}
+
+			j++;
+		}
+		
+		if(!Encontrado){
+			estadisticas.push_back(eAgregar);
+		}
+	}
+}
+*/
+
+
 void Desarrollador::setEstadisticas(vector<Estadistica *> estadist) {
     estadisticas = estadist;
 }
